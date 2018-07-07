0bdda7a829715913a753fee7486e88c8c7e19af3
Henry Winget <henrywinget@yahoo.com>
2018-07-07 18:54:01 -0500

    beginning routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import Birthdate from "./Components/Pages/Signup/Birthdate";
+import Home from "./Components/Pages/Home/";
+import FirstImpression from "./Components/Pages/JoinTheParty/FirstImpression";
+import GroupSnap from "./Components/Pages/JoinTheParty/GroupSnap";
+import Login from "./Components/Pages/Login";
+import Logout from "./Components/Pages/Logout";
+import Signup from "./Components/Pages/Signup";
+import Navigation from "./Components/Navigation";
+import Wrapper from "./Components/Wrapper";
+import { BrowserRouter, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <Birthdate />
-      </div>
-    );
-  }
-}
 
-export default App;
+export default () => (
+  <BrowserRouter>
+    <div>
+    <Wrapper>
+    <Sidebar/>
+    <Route exact path="/" component= {Home} />
+    <Route path = "/about" component = {About} />
+    <Route path = "/projects" component = {Projects} />
+    <Route path = "/contact" component = {Contact} />
+    <NavTabs />
+    </Wrapper>
+
+    </div>
+  </BrowserRouter>
+)
