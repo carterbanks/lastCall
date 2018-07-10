3c1d341539b568b04994bb87e44cf183a289e5ce
Henry Winget <henrywinget@yahoo.com>
2018-07-10 17:50:57 -0500

    run command 'yarn run dev' to sync front and back

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,16 +2,37 @@ const express = require('express');
 const router = express.Router();
 
 // User Model
-const User = require('../../models/UserSchema');
+const User = require('../../models/User');
 
-// route get request to api/items
+// route get request to api/user
 // desc get all users
-// acess public
+// access public
 
 router.get('/', (req, res) => {
   User.find()
   .sort({ date: -1})
     .then(users => res.json(users));
+});
+
+// route post request to api/user
+// desc create a user
+// access public
+
+router.post('/', (req, res) => {
+  const newUser = new User({
+    name: req.body.name
+  });
+
+  newUser.save().then(user => res.json(user));
+});
+
+// route DELETE request to api/user/:id
+// desc Delete a user
+// access public
+
+router.delete('/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then(user => user.remove().then(() => res.json({success: true}))).catch(err => res.status(404).json({success: false}));
 });
 
 module.exports=  router;
--- server.js
+++ server.js
@@ -21,6 +21,6 @@ mongoose
 //Use routes
 app.use('/api/users', users);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
