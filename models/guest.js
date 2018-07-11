803012e703b43f5a7dd233bdf90a1d76c5956835
Henry Winget <henrywinget@yahoo.com>
2018-07-10 19:04:26 -0500

    whoops

---
+++ models/guest.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Schema
+
+const GuestSchema = new Schema({
+  location: {
+    type: String,
+    required: true
+  },
+    distanceTravel: {
+    type: Number,
+    required: true
+  },
+  ageMin: {
+    type: Number,
+    required: true
+  },
+  ageMax: {
+    type: Number,
+    unique: true
+  },
+  smoking: {
+    type: Boolean,
+    default: false
+  },
+  hasAlcohol: {
+    type: Boolean,
+    default: false
+  },
+  hasFood: {
+    type: Boolean,
+    default: false
+  },
+  hasMoney: {
+    type: Boolean,
+    default: false
+  },
+  hasDD: {
+    type: Boolean,
+    default: false
+  },
+  description: {
+    type: Text,
+    required: true
+  }
+
+});
+
+module.exports = Guest = mongoose.model('guest', GuestSchema);
---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  User: require("./user"),
+  Host: require("./host"),
+  Guest: require("./guest")
+};
