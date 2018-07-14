f4aa493f06ce891c56a53aa7e7a9d9ae6634beee
Henry Winget <henrywinget@yahoo.com>
2018-07-14 11:15:09 -0500

    added DB backing for host/guest --- finished user

---
+++ client/src/components/Pages/ChooseRole/Guest/Guest.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export class Guest extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      location: "",
+      distanceTravel: 0,
+      ageMin: 0,
+      ageMax: 0,
+      smoking: false,
+      hasAlcohol: false,
+      hasFood: false,
+      hasMoney: false,
+      hasDD: false,
+      descrption: "This user did not add a description. Accept request at your own risk."
+    };
+    this.handleDateChange = this.handleDateChange.bind(this);
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    // && this.state.lastName && this.state.email && this.state.phoneNumber && this.state.location && this.state.userName && this.state.password
+    if (this.state.location && this.state.distanceTravel && this.state.ageMinb && this.state.ageMax && this.state.description) {
+      API.saveGuest({
+        location: this.state.location,
+        distanceTravel: this.state.distanceTravel,
+        ageMin: this.state.ageMin,
+        ageMax: this.state.ageMax,
+        smoking: this.state.smoking,
+        hasAlcohol: this.state.hasAlcohol,
+        hasFood: this.state.hasFood,
+        hasMoney: this.state.hasMoney,
+        hasDD: this.state.hasDD,
+        description: this.state.descrption
+      })
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  onLocationSet(data){
+    // data.description
+    // data.coords.lat
+    // data.coords.lng
+  };
+
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Guest
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,5 +4,15 @@ export default {
   // Saves a user to the database
   saveUser: function(userData) {
     return axios.post("/api/users", userData)
+  },
+
+  //Saves a guest request to the database
+  saveGuest: function(guestData) {
+    return axios.post("/api/parties/guests", guestData)
+  },
+
+  //Save a host to the database
+  saveHost: function(hostData) {
+    return axios.post("/api/parties/hosts", hostData)
   }
 };
